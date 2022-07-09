3931a6d27eb4ff4337710f8903c37869a213b5d6
farahnabil4h <80876660+farahnabil4h@users.noreply.github.com>
2022-07-09 23:00:04 +0800

    Add files via upload

---
+++ mytutor/mobile/php/payment.php
@@ -0,0 +1,40 @@
+<?php
+
+$email = $_GET['email'];
+$mobile = $_GET['mobile']; 
+$name = $_GET['name']; 
+$amount = $_GET['amount']; 
+
+
+$api_key = 'e5b905fb-f8c3-42fe-8f88-dccc42af5fc2';
+$collection_id = 'owrjh8ar';
+ 
+$host = 'https://www.billplz-sandbox.com/api/v3/bills';
+
+
+$data = array(
+          'collection_id' => $collection_id,
+          'email' => $email,
+          'mobile' => $mobile,
+          'name' => $name,
+          'amount' => ($amount + 1) * 100, // RM20
+		  'description' => 'Payment for order by '.$name,
+          'redirect_url' => "https://mytutorapplication.000webhostapp.com/mytutor/payment_update.php?email=$email&mobile=$mobile&amount=$amount&name=$name" 
+);
+
+
+$process = curl_init($host );
+curl_setopt($process, CURLOPT_HEADER, 0);
+curl_setopt($process, CURLOPT_USERPWD, $api_key . ":");
+curl_setopt($process, CURLOPT_TIMEOUT, 30);
+curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($process, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($process, CURLOPT_POSTFIELDS, http_build_query($data) ); 
+
+$return = curl_exec($process);
+curl_close($process);
+
+$bill = json_decode($return, true);
+header("Location: {$bill['url']}");
+?>
